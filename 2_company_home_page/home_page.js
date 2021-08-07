01d6e716f6aa72dad2ad50938bce8971d8eec01d
Rupesh Chiluka <rupeshchiluka@gmail.com>
2021-08-07 23:20:49 +0530

    Mobile responsive change made for welcome, search, about

--- 2_company_home_page/home_page.js
+++ 2_company_home_page/home_page.js
@@ -3,11 +3,15 @@ search = document.getElementById("search");
 searchIcon = document.getElementById("search-icon");
 
 searchBox.addEventListener("focus", function() {
-    search.style.color = "#0062ff";
-    searchIcon.style.borderBottom = "2px solid #0062ff"
+    // searchBox.style.color = "#0062ff";
+    // searchIcon.style.borderBottom = "1px solid #0062ff"
+    // searchIcon.style.color = "white";
+    // searchIcon.style.backgroundColor = "#0062ff";
 })
 
 searchBox.addEventListener("focusout", function() {
-    search.style.color = "#000000";
-    searchIcon.style.borderBottom = "0"
+    // searchBox.style.color = "#000000";
+    // searchIcon.style.borderBottom = "0"
+    // searchIcon.style.color = "black";
+    // searchIcon.style.backgroundColor = "white"
 })
