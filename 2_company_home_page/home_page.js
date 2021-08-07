72964111f7a1487c1b11274afdf6b344e7a10b4c
Rupesh Chiluka <rupeshchiluka@gmail.com>
2021-08-07 23:21:30 +0530

    Commented all lines in home_page.js

--- 2_company_home_page/home_page.js
+++ 2_company_home_page/home_page.js
@@ -2,16 +2,16 @@ searchBox = document.getElementById("search-box")
 search = document.getElementById("search");
 searchIcon = document.getElementById("search-icon");
 
-searchBox.addEventListener("focus", function() {
-    // searchBox.style.color = "#0062ff";
-    // searchIcon.style.borderBottom = "1px solid #0062ff"
-    // searchIcon.style.color = "white";
-    // searchIcon.style.backgroundColor = "#0062ff";
-})
+// searchBox.addEventListener("focus", function() {
+//     searchBox.style.color = "#0062ff";
+//     searchIcon.style.borderBottom = "1px solid #0062ff"
+//     searchIcon.style.color = "white";
+//     searchIcon.style.backgroundColor = "#0062ff";
+// })
 
-searchBox.addEventListener("focusout", function() {
-    // searchBox.style.color = "#000000";
-    // searchIcon.style.borderBottom = "0"
-    // searchIcon.style.color = "black";
-    // searchIcon.style.backgroundColor = "white"
-})
+// searchBox.addEventListener("focusout", function() {
+//     searchBox.style.color = "#000000";
+//     searchIcon.style.borderBottom = "0"
+//     searchIcon.style.color = "black";
+//     searchIcon.style.backgroundColor = "white"
+// })
